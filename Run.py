6f359b729260422b53bbe22cc15ca3a9d0479fd7
MR. SUJOY <110232390+sujoyroy113@users.noreply.github.com>
2023-01-07 23:26:57 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,1 +1,14 @@
-
+import os,time,platform
+print('\033[1;32m\n Congratulations! Your Device Support This Tools\033[1;32m')
+time.sleep(0.5)
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+print(f'{red}[•] Join Over Facebook Group {white}')
+os.system('xdg-open https://www.facebook.com/groups/669702607987139//')
+time.sleep(0.05)
+import free_SXS
+free_SXS.Main() 
